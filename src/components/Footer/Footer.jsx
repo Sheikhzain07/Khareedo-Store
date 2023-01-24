0e0d600599a282a620ebcf442565a01e3c21bf8e
Sheikhzain07 <sheikhzain459@gmail.com>
2023-01-25 04:23:29 +0530

    footer completed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,124 @@
 import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./footer.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <h1 className="text-white">Khareedo Store</h1>
+            </div>
+            <p className="footer__text mt-4">
+              An E-commerce website,We have high quality products with unique
+              design and structure. Also we have best built and strong interior
+              products which add an extra value in your home and life.
+            </p>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup mb-3>
+                {/* ListGroup & ListGroupItem is like 'ul' and 'li' */}
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"#"}>Mobile Phone</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"#"}>Modern Sofa</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"#"}>Arm Chair</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"#"}>Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup mb-3>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"/shop"}>Shop</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"/cart"}>Cart</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"login"}>Login</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to={"#"}>Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup mb-3 className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>Bahraich, U.P. , India</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-linkedin-box-fill"></i>
+                  </span>
+                  <p>
+                    <a
+                      target="_blank"
+                      href="https://www.linkedin.com/in/sheikh-zainuddin-10ba54211/"
+                    >
+                      Sheikh Zainudin
+                    </a>
+                  </p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>sheikhzain459@gmail.com</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-github-fill"></i>
+                  </span>
+                  <p>
+                    <a target="_blank" href="https://github.com/Sheikhzain07">
+                      Sheikhzain07
+                    </a>
+                  </p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="footer__copyright text-center">
+              <i class="ri-copyright-line"></i> Copyright {year} developed by
+              Sheikh Zainuddin<i class="ri-service-line"></i>. All rights
+              reserved.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
 export default Footer;

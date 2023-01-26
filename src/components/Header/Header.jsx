8ceec494acfc025f9ea5fdc76426ab5aaf498243
Sheikhzain07 <sheikhzain459@gmail.com>
2023-01-27 01:55:53 +0530

    real-time quantity of product shown on navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import { Container, Row } from "reactstrap";
 import logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 import { motion } from "framer-motion";
+import { useSelector } from "react-redux";
 
 const nav__links = [
   {
@@ -23,6 +24,7 @@ const nav__links = [
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const stickyHeaderFun = () => {
     window.addEventListener("scroll", () => {
@@ -81,7 +83,7 @@ const Header = () => {
               </span>
               <span className="cart__icon">
                 <i className="ri-shopping-bag-line"></i>
-                <span className="badge">1</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
               <span>
                 <motion.img

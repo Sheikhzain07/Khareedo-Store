3a2df0d7e0ba92f5d9a1a9326d00c19533dff26e
Sheikhzain07 <sheikhzain459@gmail.com>
2023-01-31 00:42:25 +0530

    login form layout created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import TitleOfApp from "../components/titleApp/TitleOfApp";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <TitleOfApp title="Login">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center">
+              <h3 className="fw-bold fs-4">Login</h3>
+
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input type="email" placeholder="Enter your Email" />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="password" placeholder="Enter your Password" />
+                </FormGroup>
+                <button className="but_btn auth_btn">Login</button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </TitleOfApp>
+  );
 }
 
 export default Login;
